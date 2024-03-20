7d488fb601dc3ba59b7a6e2e023157627b8c4951
100109 <emcis44@gmail.com>
2024-03-20 15:42:32 +0900

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>메인화면</main>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  background: gray;
+  height: 100vh;
+  width: 100%;
+  position: relative;
+  display: block;
+}
+</style>
---
+++ src/views/product/ShoppingView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>쇼핑하기</div>
+</template>
+
+<script>
+export default {
+  name: "Shopping",
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  background: gray;
+  height: 100vh;
+  width: 100%;
+  position: relative;
+  display: block;
+}
+</style>
