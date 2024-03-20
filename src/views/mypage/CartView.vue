038f0e287e62bb19de9e49cd04be810dbee3de09
100109 <emcis44@gmail.com>
2024-03-20 17:06:39 +0900

    240320_진솔_1

--- src/views/mypage/CartView.vue
+++ src/views/mypage/CartView.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="row">
-    <section-title title="장바구니" sub-title="장바구니내용" />
+    <section-title title="장바구니" sub_title="장바구니내용" />
     <cart-list />
   </main>
 </template>
