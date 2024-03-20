c279f035b78f9d990f8deb107ec41313e9f8b030
junhyok7146 <junhyeok_an@naver.com>
2024-03-20 18:22:09 +0900

    0320수정완료

--- src/components/HomeSection/BestProductSection.vue
+++ src/components/HomeSection/BestProductSection.vue
@@ -8,12 +8,21 @@
                         <img src="/image/RDV_m1_p14.png" alt="">
                     </router-link>
                     <div class="detail">
-                        <p>
-                            [지엠팜더칼슘디]/1개월분 (유소아 / 어린이 칼슘) 칼슘 마그네슘
-                        </p>
-                        <p>
-                            25,000원
-                        </p>
+                        <div class="infoBox_1">
+                            <div><img src="/image/custom_2716877566803974.png" alt=""></div>
+                            <div><img src="/image/custom_1116487034051360.png" alt=""></div>
+                            <p>
+                                [지엠팜더칼슘디]/1개월분 (유소아 / 어린이 칼슘) 칼슘 마그네슘
+                            </p>
+                        </div>
+                       <div class="infoBox_2">
+                            <p>
+                                25,000원
+                            </p>
+                            <div v-for="(item,index) in images" :key="index">
+                                <img :src="item.img" :alt="item.alt">
+                            </div>
+                       </div>
                     </div>
                 </div>
                 <div class="best">
@@ -49,6 +58,13 @@ import SectionTitle from "@/components/layout/SectionTitle.vue"
         components:{
         SectionTitle,
     },
+    data(){
+        return{
+            images:[
+                {img:'./image/prd_list_cart.png', imgBF : './image/prd_list_cart.png', imgAF:'prd_list_cart_h.png', alt:'장바구니'}
+            ]
+        }
+    }
     }
 </script>
 
@@ -63,11 +79,24 @@ import SectionTitle from "@/components/layout/SectionTitle.vue"
             border-radius: 15px;
             text-align: center;
             padding: 30px;
+            img{
+                padding: 30px;
+            }
             .detail{
                 border: 1px solid #fff;
                 background: #fff;
                 border-radius: 15px;
                 padding: 30px;
+                .infoBox_1{
+                    display: flex;
+                    flex-wrap: wrap;
+                    align-items: flex-end;
+                }
+                .infoBox_2{
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: flex-end
+                }
             }
         }
         .best{
